ba282c425d8b0f5ea515b7e47e44eaf35bcd63bc
Adambomb2022 <129651600+Adambomb2022@users.noreply.github.com>
2023-05-13 21:13:27 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,14 +1,14 @@
 public class Test{
     public static void main(String[] args){ // test made by Adam B
         double[][] data = {{1,0,2,0,3},{0,0,0,0,1},{0,0,0,0,0},{4,0,0,4,4},{0,0,5,6,7}};
-        Matrix n1 = new Matrix(10,10);
+        Matrix n1 = new Matrix(10,10); // edit me
         n1.set(3,3,50);
         n1.set(9,2,49);
         n1.set(9,1,48);
         n1.set(9,3,49);
         n1.set(0,0,1);
         n1.set(9,1,44);
-        Matrix n2 = new Matrix(data);
+        Matrix n2 = new Matrix(data); // edit me 
         n2.set(0,1,1);
         n2.set(1,4,2);
         System.out.println(n2.get(0,0) == 1);
